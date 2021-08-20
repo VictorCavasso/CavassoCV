88f94580c815a2b6222dc414adbb6713ba685480
Cavasso <victorcavasso@hotmail.com>
2021-08-20 03:05:13 -0300

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = ({ currentDate }) => {
+  return (
+    <div>
+      <p className='text-center my-8 py-4 border-t-2'>
+        This website was built during Fullstack Master classes. We used NextJS + SSR (Server Side Rendering) + Vercel (as platform)<br />
+      </p>
+      <p className='text-center my-8 py-4 border-t-2'>Latest version: {currentDate}</p>
+    </div>
+  )
+}
+export default Footer
